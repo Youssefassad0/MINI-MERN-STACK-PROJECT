70e78c2ef4097baed77988ba4fba9dfc1b2e067c
assadyoussef0 <amine150assad@gmail.com>
2024-04-22 15:32:56 +0100

    first commit with some routes

---
+++ backend/models/BookModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const BookShema = mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  publishYear: { type: Number, required: true },
+},{
+    timestamps:true
+});
+
+export const Book = mongoose.model("Book", BookShema);
