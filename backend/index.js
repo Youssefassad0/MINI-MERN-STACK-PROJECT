7920bd521a0c550372ed97daf885bff4c07b7b59
assadyoussef0 <amine150assad@gmail.com>
2024-04-22 16:28:41 +0100

    add router file and complet all crud routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/BookModel.js";
-
+import BooksRoute from './routes/BookRoute.js'
 const app = express();
 
 app.use(express.json());
@@ -10,65 +10,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   return res.status(234).send("Welcome To Book Project");
 });
-
-app.post("/books", async (req, res) => {
-  try {
-    if (!req.body.title || !req.body.author || !req.body.publishYear) {
-      return res.status(400).send({
-        message: "Send All required fields: title, author, publishYear",
-      });
-    }
-    const newBook = {
-      title: req.body.title,
-      author: req.body.author,
-      publishYear: req.body.publishYear,
-    };
-    const book = await Book.create(newBook);
-    return res.status(201).send(book);
-  } catch (err) {
-    console.log(err.message);
-    return res.status(500).send({ message: "Internal Server Error" });
-  }
-});
-app.get("/books", async (req, res) => {
-  try {
-    const books = await Book.find({});
-    return res.status(200).json({
-      count: books.length,
-      data: books,
-    });
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-app.get("/books/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const book = await Book.findById(id);
-    return res.status(200).json(book);
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-app.put('/books/:id',async (req,res)=>{
-    try{
-        if (!req.body.title || !req.body.author || !req.body.publishYear) {
-            return res.status(400).send({
-              message: "Send All required fields: title, author, publishYear",
-            });
-          }
-
-          const {id} = req.params;
-          const result = await Book.findByIdAndUpdate(id,req.body); 
-          if(!result){
-            return res.status(404).json({message:'Book Not Found'});
-          }
-
-    }
-    catch(err){
-        console.log(err.message);
-    }
-})
+app.use('/books',BooksRoute)
 
 
 mongoose
